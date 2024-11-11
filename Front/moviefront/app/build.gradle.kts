226f83abe9efbca640c21fe48d2f11a0f66ac86f
devloppeurtn <houssemhfaissi8@gmail.com>
2024-11-12 00:35:24 +0100

    add dependecies

--- Front/moviefront/app/build.gradle.kts
+++ Front/moviefront/app/build.gradle.kts
@@ -62,7 +62,8 @@ dependencies {
     implementation("io.ktor:ktor-client-content-negotiation:2.3.0")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-    implementation("io.coil-kt:coil:2.1.0")
     implementation("com.google.code.gson:gson:2.8.9")
+    implementation("io.coil-kt:coil:2.1.0")
+
 
 }
