3cd061dbbdd29ab5af5a1b2da1bffcc1cfd3b9a3
devloppeurtn <houssemhfaissi8@gmail.com>
2024-11-17 12:10:18 +0100

    configuration getaway

---
+++ back/Getaway-service/Getaway-service/src/main/java/com/example/Getaway_service/GetawayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Getaway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetawayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetawayServiceApplication.class, args);
+	}
+
+}
