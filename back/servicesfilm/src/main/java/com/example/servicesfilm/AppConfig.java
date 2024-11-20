cf975dc5cf99d42528c64395101e50fc3d1f9c9a
devloppeurtn <houssemhfaissi8@gmail.com>
2024-11-20 02:21:11 +0100

    reste de derniere commit

--- back/servicesfilm/src/main/java/com/example/servicesfilm/AppConfig.java
+++ back/servicesfilm/src/main/java/com/example/servicesfilm/AppConfig.java
@@ -1,12 +1,16 @@
 package com.example.servicesfilm;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-@Configuration  // Ajoutez cette annotation
 
+
+@Configuration
 public class AppConfig {
+
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
